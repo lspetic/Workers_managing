d68fa45542455b7d93ee11147980e4186776e769
Lovro <lspetic@riteh.hr>
2018-06-13 04:42:16 +0200

    2.5

--- app/src/main/java/com/example/eri/workers_managing/User_spec.java
+++ app/src/main/java/com/example/eri/workers_managing/User_spec.java
@@ -3,14 +3,22 @@ package com.example.eri.workers_managing;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.eri.workers_managing.model.User;
 
 public class User_spec extends AppCompatActivity{
     private User user;
-
+    private TextView mTvName;
+    private TextView mTvEmail;
+    private TextView mTvProf;
+    private TextView mTvSurname;
+    private TextView mTvAddress;
+    private TextView mTvPhone;
     protected void onCreate(Bundle savedInstanceState){
         Bundle extras = getIntent().getExtras();
+        setContentView(R.layout.myprofile2);
         super.onCreate(savedInstanceState);
         try {
 
@@ -22,6 +30,31 @@ public class User_spec extends AppCompatActivity{
             Log.d("++error++",e.toString());
 
         }
+        mTvName =  findViewById(R.id.tv_name);
+        mTvEmail =  findViewById(R.id.tv_email);
+
+        mTvProf =  findViewById(R.id.tv_prof);
+        // mTvSurname =  findViewById(R.id.tv_surname);
+        mTvAddress =  findViewById(R.id.tv_address);
+        mTvPhone =  findViewById(R.id.tv_phone);
+//        mTvTimeS =  findViewById(R.id.tv_vremena1);
+//        mTvTimeE =  findViewById(R.id.tv_vremena2);
+//        mTvGradiliste =findViewById(R.id.tv_grad);
+        mTvAddress = findViewById(R.id.tv_address);
+        mTvPhone = findViewById(R.id.et_phone);
+        mTvSurname=findViewById(R.id.et_surname);
+
+        mTvPhone.setVisibility(View.VISIBLE);
+        mTvAddress.setVisibility(View.VISIBLE);
+
+        mTvName.setText(user.getName());
+        mTvEmail.setText(user.getEmail());
+        mTvSurname.setText(user.getSurname());
+        mTvPhone.setText(user.getPhone());
+        mTvAddress.setText(user.getAddress());
+        mTvProf.setText(user.getProfession());
+
+
 if(user!=null){
     Log.d("++++",user.getName());
 }
@@ -35,5 +68,7 @@ try{
 
 
     }
+
+
 
 }
